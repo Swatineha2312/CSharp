b1af9d78ed54cc4f33d3a417ae44839a25148846
Swatineha2312 <swatineha2312@gmail.com>
2020-11-12 14:13:03 +1100

    Enum e.g.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace HelloWorld
+{
+    public enum ShippingMethod
+    {
+        RegularAirMail=1,
+        RegisteredAirMail=2,
+        Express=3
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method = ShippingMethod.Express;
+            Console.WriteLine((int)method);
+        }
+    }
+}
